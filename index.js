8845fc73eb397b0039cbace24bbf21b38b675330
satish yachwad <yachwadsatish71@gmail.com>
2026-07-02 23:35:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.get("/", (req, res) => {
+  res.send("satish reddy become a fullstack deveoper");
+});
+app.get("/login", (req, res) => {
+  res.json({ username: "satish", password: "satish reddy" });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server running successfully ${port}`);
+});
